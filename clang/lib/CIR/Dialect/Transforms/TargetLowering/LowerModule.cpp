1c75eda1d6368ae371ea1ed34c0aca4c321d64f4
seven-mile <i@7li.moe>
2024-11-13 17:52:49 +0000

    [CIR][Transforms] Fix undesirable include of upstream

--- clang/lib/CIR/Dialect/Transforms/TargetLowering/LowerModule.cpp
+++ clang/lib/CIR/Dialect/Transforms/TargetLowering/LowerModule.cpp
@@ -11,9 +11,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-// FIXME(cir): This header file is not exposed to the public API, but can be
-// reused by CIR ABI lowering since it holds target-specific information.
-#include "../../../../Basic/Targets.h"
 #include "clang/Basic/LangOptions.h"
 #include "clang/Basic/TargetOptions.h"
 
